218a49ef29677cbe3a908b0ffb5054e0f8647ff0
tnguyen217 <tnguyen217@nait.ca>
2022-04-06 03:34:25 -0600

    add edit/delete btn and styled-compoments to ProductCard elements

--- src/components/products/ProductCard/ProductCard.js
+++ src/components/products/ProductCard/ProductCard.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import {ProductCardStyles} from './styles'
+import { IconButton } from 'ui/buttons';
+import { ProductCardStyles, ProductImage, ProductName, ProductPrice, ProductDes } from './styles'
 
 function ProductCard ({children, id, name, price, des, url, ...props})  {
   // console.log(name)
 
   return (
-        <div>
-          <h3>{name}</h3>
-          <p>{price}</p>
-          <p>{des}</p>
+      <ProductCardStyles>
+        <ProductImage className='product-img'>
           <img src={url} alt={name} />
+        </ProductImage>
+
+        <section>
+          <ProductName>{name}</ProductName>
+          
+          <ProductPrice>{price}</ProductPrice>
+          
+          <ProductDes>{des}</ProductDes>
+        </section>
+        
+        <div>
+          <IconButton>Edit</IconButton>
+          <IconButton>Delete</IconButton>
         </div>
+      </ProductCardStyles>
   )
 }
 
--- src/components/products/ProductCard/styles.js
+++ src/components/products/ProductCard/styles.js
@@ -3,11 +3,24 @@ import styled from 'styled-components';
 
 
 const ProductCardStyles  = styled.div`
-      /* styles */
-      h2{
-          font-size:1.75rem;
-          color:#374151;
-      }
+      
 `;
 
-export {ProductCardStyles}
+const ProductImage  = styled.div`
+      
+`;
+
+const ProductName = styled.h3`
+  
+`;
+
+const ProductPrice = styled.p`
+  
+`;
+
+const ProductDes = styled.p`
+  
+`;
+
+
+export { ProductCardStyles, ProductImage, ProductName, ProductPrice, ProductDes }
